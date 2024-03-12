bebde3ff515dbc984a7935e5195491927ca5d3ac
Nick Martin <nickm1988@hotmail.co.uk>
2024-03-12 21:27:16 +0000

    Exercises 1.1.-1.2.

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,27 +1,43 @@
-const Hello = (props) => {
-
-  console.log(props)
+const Header = (props) => {
   return (
     <div>
-      <p>
-
-        Hello {props.name}, you are {props.age} years old
-      </p>
+      <h1>{props.course}</h1>
     </div>
   )
 }
 
-const App = () => {
+const Content = (props) => {
+  return (
+    <div>
+      <p>Section: '{props.content}' has {props.exercises} exercises.</p>
+    </div>
+  )
+}
 
-  const name = 'Peter'
-  const age = 10
+const Total = (props) => {
+  return (
+    <div>
+      <p>Number of exercises: {props.total}</p>
+    </div>
+  )
+}
+
+
+const App = () => {
+  const content = [
+    { name: 'part1', exercises: 10},
+    { name: 'part2', exercises: 7},
+    { name: 'part3', exercises: 14},
+  ]
 
   return (
     <div>
-      <h1>Greetings</h1>
-
-      <Hello name='Maya' age={26 + 10} />
-      <Hello name={name} age={age} />
+      <Header course='Half Stack application development' />
+      <Content content={content[0].name} exercises={content[0].exercises}/>
+      <Content content={content[1].name} exercises={content[1].exercises}/>
+      <Content content={content[2].name} exercises={content[2].exercises}/>
+      <Total total={content[0].exercises+content[1].exercises+content[2].exercises} />
+      
     </div>
   )
 }
